9b3580375a15a602357d8b3658cc4eed72ab5ab4
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-31 12:43:14 +0100

    meetings

--- src/app/classes/meeting.ts
+++ src/app/classes/meeting.ts
@@ -1,2 +1,6 @@
 export class Meeting {
+  id: number;
+  titre: string;
+  date_deb: Date;
+  date_fin: Date;
 }
--- src/app/services/meeting.service.ts
+++ src/app/services/meeting.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Meeting } from '../classes/meeting';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MeetingService {
-
+  static meetings: Meeting[] = new Array();
   constructor() { }
+  additem(item) {
+    MeetingService.meetings.push(item);
+
+
+
+  }
 }
