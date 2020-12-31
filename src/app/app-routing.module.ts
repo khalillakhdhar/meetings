07ab42ab736ed58584068367f3e4096d3bb5570a
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-31 13:02:22 +0100

    meetings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListMeetingsComponent } from './list-meetings/list-meetings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: ListMeetingsComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/list-meetings/list-meetings.component.ts
+++ src/app/list-meetings/list-meetings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MeetingService } from '../services/meeting.service';
 
 @Component({
   selector: 'app-list-meetings',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListMeetingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api:MeetingService) { }
 
   ngOnInit(): void {
   }
