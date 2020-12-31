2ea09c41134bd86699002759a5c708d83478fac6
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-31 12:54:47 +0100

    components

---
+++ src/app/details-meeting/details-meeting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-meeting',
+  templateUrl: './details-meeting.component.html',
+  styleUrls: ['./details-meeting.component.css']
+})
+export class DetailsMeetingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/list-meetings/list-meetings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-meetings',
+  templateUrl: './list-meetings.component.html',
+  styleUrls: ['./list-meetings.component.css']
+})
+export class ListMeetingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
